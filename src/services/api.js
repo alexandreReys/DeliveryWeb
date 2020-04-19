957fd9b0f91469cd6246a0fec9bbb095d45c40ab
Alexandre Reys <anr.alexandre@gmail.com>
2020-04-18 21:05:59 -0300

    .

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,7 @@
 import axios from "axios";
 import * as loginService from "./loginService";
 
-const environment = process.env.REACT_APP_ENV;
-
-let mysqlBaseUrl;
-if (environment === "development") {
-  mysqlBaseUrl = process.env.REACT_APP_DEV_BASE_URL;
-} else if (environment === "production") {
-  mysqlBaseUrl = process.env.REACT_APP_PROD_BASE_URL;
-} else {
-  mysqlBaseUrl = "http://motoguardian-com-br.umbler.net";
-}
+const mysqlBaseUrl = process.env.REACT_APP_BASE_URL;
 
 export const api = axios.create({
   baseURL: mysqlBaseUrl,
