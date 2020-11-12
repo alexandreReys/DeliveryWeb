41cd8561067ed2aea471be14bf49f5f565e0abe6
unknown <anr.alexandre@gmail.com>
2020-11-12 00:16:14 -0300

    .

--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,29 @@
 import axios from "axios";
-
 import * as loginService from "./loginService";
+import * as envJson from "../.env.json";
 
 const mysqlBaseUrl =
-  process.env.REACT_APP_BASE_URL || "https://motoguardian-api.herokuapp.com";
+    process.env.NODE_ENV === "development"
+        ? envJson.REACT_APP_BASE_URL
+        : process.env.REACT_APP_BASE_URL;
+
+
+// const mysqlBaseUrl =
+//     process.env.REACT_APP_BASE_URL || "https://motoguardian-api.herokuapp.com";
 
 export const api = axios.create({
-  baseURL: mysqlBaseUrl,
+    baseURL: mysqlBaseUrl,
 });
 
+console.log("envJson.REACT_APP_BASE_URL", envJson.REACT_APP_BASE_URL);
+console.log("process.env.NODE_ENV ", process.env.NODE_ENV);
+console.log("process.env.REACT_APP_BASE_URL", process.env.REACT_APP_BASE_URL);
 console.log("mysqlBaseUrl", mysqlBaseUrl);
 
 api.interceptors.request.use(async (config) => {
-  const token = loginService.getToken();
-  if (token) {
-    config.headers.authorization = `${token}`;
-  }
-  return config;
+    const token = loginService.getToken();
+    if (token) {
+        config.headers.authorization = `${token}`;
+    }
+    return config;
 });
