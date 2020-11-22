0bffe2eca3c1d6a9871eb7772446cb4c151e5df7
unknown <anr.alexandre@gmail.com>
2020-11-22 16:52:53 -0300

    .

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import * as loginService from "./loginService";
 import * as envJson from "../.env.json";
 
+console.log("============================================================");
 console.log("process.env.NODE_ENV ", process.env.NODE_ENV);
 
 const mysqlBaseUrl =
@@ -9,14 +10,18 @@ const mysqlBaseUrl =
         ? envJson.REACT_APP_BASE_URL
         : process.env.REACT_APP_BASE_URL;
 
+console.log("============================================================");
 console.log("envJson.REACT_APP_BASE_URL", envJson.REACT_APP_BASE_URL);
+console.log("============================================================");
 console.log("process.env.REACT_APP_BASE_URL", process.env.REACT_APP_BASE_URL);
 
 export const api = axios.create({
     baseURL: mysqlBaseUrl,
 });
 
+console.log("============================================================");
 console.log("mysqlBaseUrl", mysqlBaseUrl);
+console.log("============================================================");
 
 api.interceptors.request.use(async (config) => {
     const token = loginService.getToken();
