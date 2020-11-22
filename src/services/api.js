568d0164e9c31c358ab8987613b3083efc57d255
unknown <anr.alexandre@gmail.com>
2020-11-22 16:01:43 -0300

    .

--- src/services/api.js
+++ src/services/api.js
@@ -2,22 +2,20 @@ import axios from "axios";
 import * as loginService from "./loginService";
 import * as envJson from "../.env.json";
 
+console.log("process.env.NODE_ENV ", process.env.NODE_ENV);
+
 const mysqlBaseUrl =
     process.env.NODE_ENV === "development"
         ? envJson.REACT_APP_BASE_URL
         : process.env.REACT_APP_BASE_URL;
 
-
-// const mysqlBaseUrl =
-//     process.env.REACT_APP_BASE_URL || "https://motoguardian-api.herokuapp.com";
+console.log("envJson.REACT_APP_BASE_URL", envJson.REACT_APP_BASE_URL);
+console.log("process.env.REACT_APP_BASE_URL", process.env.REACT_APP_BASE_URL);
 
 export const api = axios.create({
     baseURL: mysqlBaseUrl,
 });
 
-// console.log("envJson.REACT_APP_BASE_URL", envJson.REACT_APP_BASE_URL);
-// console.log("process.env.NODE_ENV ", process.env.NODE_ENV);
-// console.log("process.env.REACT_APP_BASE_URL", process.env.REACT_APP_BASE_URL);
 console.log("mysqlBaseUrl", mysqlBaseUrl);
 
 api.interceptors.request.use(async (config) => {
